237ee1084579ddc3b8d9b75db048df13c55db098
RicardoMVA <ricardovalenca@gmail.com>
2020-01-20 19:52:30 -0300

    initial commit - dependencies setup

---
+++ src/test/java/com/qualiti/odontoSystem/OdontoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualiti.odontoSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
