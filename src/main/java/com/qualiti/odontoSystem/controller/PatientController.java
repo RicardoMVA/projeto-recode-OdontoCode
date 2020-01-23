ecba4e31926a9ab375320b5999cd82dc8d7f4a5c
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-01-23 18:36:19 -0300

    GET without Id in PatientController

--- src/main/java/com/qualiti/odontoSystem/controller/PatientController.java
+++ src/main/java/com/qualiti/odontoSystem/controller/PatientController.java
@@ -1,5 +1,24 @@
 package com.qualiti.odontoSystem.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qualiti.odontoSystem.service.PatientService;
+
+@RestController()
+@RequestMapping("/api/v1/patients")
 public class PatientController {
 
+	private PatientService patientService;
+	PatientController(PatientService patientService) {
+		this.patientService = patientService;
+	}
+	
+	@GetMapping
+	public List findAll() {
+		return patientService.findAll();
+	}
 }
