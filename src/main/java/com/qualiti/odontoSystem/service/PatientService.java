0d4b2330058cded95469ed45e832d336c61be6d4
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-01-23 18:10:21 -0300

    findAll method

--- src/main/java/com/qualiti/odontoSystem/service/PatientService.java
+++ src/main/java/com/qualiti/odontoSystem/service/PatientService.java
@@ -1,5 +1,21 @@
 package com.qualiti.odontoSystem.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qualiti.odontoSystem.repository.PatientRepository;
+
+@Service
 public class PatientService {
 
+	private PatientRepository patientRepository;
+
+	PatientService(PatientRepository patientRepository) {
+		this.patientRepository = patientRepository;
+	}
+	
+	public List findAll() {
+		return patientRepository.findAll();
+	}
 }
