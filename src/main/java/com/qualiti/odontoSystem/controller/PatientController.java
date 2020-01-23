ad3bde28777ce64f95656b537b14eac6e37adb63
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-01-23 18:42:22 -0300

    API DELETE in PatientController

--- src/main/java/com/qualiti/odontoSystem/controller/PatientController.java
+++ src/main/java/com/qualiti/odontoSystem/controller/PatientController.java
@@ -3,7 +3,9 @@ package com.qualiti.odontoSystem.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,5 +55,12 @@ public class PatientController {
 		} else {
 			return ResponseEntity.ok().body(updatedPatient);
 		}
+	}
+	
+	@DeleteMapping(path ={"/{id}"})
+	public ResponseEntity delete(@PathVariable long id) {
+			patientService.delete(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+			
 	}
 }
