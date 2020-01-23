f47d280d5c7ebce8a64858720be6a44d83ee5352
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-01-23 18:41:08 -0300

    API PUT in PatientController

--- src/main/java/com/qualiti/odontoSystem/controller/PatientController.java
+++ src/main/java/com/qualiti/odontoSystem/controller/PatientController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +43,15 @@ public class PatientController {
 	@PostMapping
 	public Patient create(@RequestBody Patient patient) {
 		return patientService.create(patient);
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity update(@PathVariable("id") long id, @RequestBody Patient patient) {
+		Patient updatedPatient = patientService.update(id, patient);
+		if (updatedPatient == null) {
+			return ResponseEntity.notFound().build();			
+		} else {
+			return ResponseEntity.ok().body(updatedPatient);
+		}
 	}
 }
