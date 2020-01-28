d42e4e984e7314d3ee89210c6996ff88e106dd0c
RicardoMVA <ricardovalenca@gmail.com>
2020-01-28 19:03:28 -0300

    slight modification to js logic

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,12 +1,10 @@
-document.querySelector("#fetch").addEventListener("click", (e) => {
-	fetch(`/api/v1/patients`)
-	  .then((response) => {
-	    return response.json();
-	  })
-	  .then((dbResponse) => {
-	  	document.querySelector("#result").innerHTML = JSON.stringify(dbResponse);	
-	  })
-	  .catch((err) => {
-	    console.log(err);
-	  });
-})
+fetch(`/api/v1/patients`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((dbResponse) => {
+  	document.querySelector("#result").innerHTML = JSON.stringify(dbResponse);	
+  })
+  .catch((err) => {
+    console.log(err);
+  });
