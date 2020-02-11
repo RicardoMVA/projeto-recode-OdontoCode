24311f727a3a6cb99687d38246941a7ecc3432c4
jonaslucenafilho <jonaslucenafilho@gmail.com>
2020-02-11 18:08:32 -0300

    add validation annotations to 'appointment'

--- src/main/java/com/qualiti/odontoSystem/model/Appointment.java
+++ src/main/java/com/qualiti/odontoSystem/model/Appointment.java
@@ -12,6 +12,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -21,18 +25,31 @@ public class Appointment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
+	@NotNull(message = "O campo paciente não pode ser nulo")
+	@NotEmpty(message = "É necessário selecionar um nome.")
 	@JsonBackReference
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "patient_id", nullable = false)
 	private Patient patient;
 
+	@NotNull(message = "A data não pode ser nula.")
+	@NotEmpty(message = "É necessário preencher a data da consulta.")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date;
 
+	@NotNull(message = "O valor da consulta não pode ser nulo.")
+	@NotEmpty(message = "É necessário preencher o valor da consulta.")
+	@Size(min = 4, max = 7, message = "Valor da consulta com tamanho inválido.")
+//	blocks all except numbers
+	@Pattern(regexp = "[0-9]*", message = "Valor da consulta com caractere(s) inválido(s).")
 	@Column
 	private int value;
 
+	@NotNull(message = "A especialidade não pode ser nula.")
+	@NotEmpty(message = "É necessário preencher uma especialidade.")
+//	blocks all numbers and non-latin characters
+	@Pattern(regexp = "[^0-9]*[^\\P{L}]*", message = "A especialidae preenchida contém caracteres inválidos.")
 	@Column
 	private String specialty;
 
